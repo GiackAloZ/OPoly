4c71342f32560c8adc81c4d3490ec44ebd98821b
Giacomo Aloisi <giacomo.aloisi1998@gmail.com>
2020-12-31 12:03:39 +0100

    Implement check perfectly nested loop

--- opoly/checks.py
+++ opoly/checks.py
@@ -1,4 +1,9 @@
 from opoly.statements import ForLoopStatement
 
+
 def check_perfectly_nested_loop(loop: ForLoopStatement) -> bool:
-    raise NotImplementedError()
+    if len(loop.body) > 1:
+        return not any([isinstance(st, ForLoopStatement) for st in loop.body])
+    if isinstance(loop.body[0], ForLoopStatement):
+        return check_perfectly_nested_loop(loop.body[0])
+    return True
