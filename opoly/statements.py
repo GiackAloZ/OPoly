94f9938f5ac4a9594c80d9723de005abfeacbf15
Giacomo Aloisi <giacomo.aloisi1998@gmail.com>
2021-01-02 23:23:36 +0100

    Refactor checks

--- opoly/statements.py
+++ opoly/statements.py
@@ -111,3 +111,18 @@ class ForLoopStatement(BlockStatement):
         step = "" if isinstance(self.step, ConstantExpression) or self.step.value == 1 \
             else f" step {self.step}"
         return f"{head}{step}"
+
+
+def check_perfectly_nested_loop(loop: ForLoopStatement) -> bool:
+    if len(loop.body) > 1:
+        return not any([isinstance(st, ForLoopStatement) for st in loop.body])
+    if isinstance(loop.body[0], ForLoopStatement):
+        return check_perfectly_nested_loop(loop.body[0])
+    return True
+
+
+def check_plain_nested_loop(loop: ForLoopStatement) -> bool:
+    for stmt in loop.body:
+        if isinstance(stmt, ForLoopStatement) and not check_plain_nested_loop(stmt):
+            return False
+    return loop.is_plain()
--- tests/test_statements.py
+++ tests/test_statements.py
@@ -11,7 +11,9 @@ from opoly.expressions import (
 from opoly.statements import (
     StatementType,
     AssigmentStatement,
-    ForLoopStatement
+    ForLoopStatement,
+    check_perfectly_nested_loop,
+    check_plain_nested_loop
 )
 
 
@@ -131,3 +133,77 @@ class TestForLoopStatement():
                                  lowerbound=ConstantExpression(1),
                                  upperbound=ConstantExpression(10)
                                  )
+
+    def test_perfectly_nested_loop(self):
+        inner_loop = ForLoopStatement(
+            body=[AssigmentStatement(VariableExpression("a", [VariableExpression("j")]),
+                                     VariableExpression("b", [VariableExpression("j")]))],
+            index=VariableExpression("j"),
+            lowerbound=ConstantExpression(1),
+            upperbound=VariableExpression("M")
+        )
+
+        outer_loop = ForLoopStatement(
+            body=[inner_loop],
+            index=VariableExpression("i"),
+            lowerbound=ConstantExpression(1),
+            upperbound=VariableExpression("N")
+        )
+
+        assert check_perfectly_nested_loop(outer_loop)
+
+    def test_not_perfectly_nested_loop(self):
+        inner_loop = ForLoopStatement(
+            body=[AssigmentStatement(VariableExpression("a", [VariableExpression("j")]),
+                                     VariableExpression("b", [VariableExpression("j")]))],
+            index=VariableExpression("j"),
+            lowerbound=ConstantExpression(1),
+            upperbound=VariableExpression("M")
+        )
+
+        outer_loop = ForLoopStatement(
+            body=[AssigmentStatement(VariableExpression("a", [VariableExpression("i")]),
+                                     VariableExpression("b", [VariableExpression("i")])),
+                  inner_loop],
+            index=VariableExpression("i"),
+            lowerbound=ConstantExpression(1),
+            upperbound=VariableExpression("N")
+        )
+
+        assert not check_perfectly_nested_loop(outer_loop)
+
+    def test_plain_nested_loop(self):
+        inner_loop = ForLoopStatement(
+            body=[AssigmentStatement(VariableExpression("a", [VariableExpression("j")]),
+                                     VariableExpression("b", [VariableExpression("j")]))],
+            index=VariableExpression("j"),
+            lowerbound=ConstantExpression(1),
+            upperbound=VariableExpression("M")
+        )
+
+        outer_loop = ForLoopStatement(
+            body=[inner_loop],
+            index=VariableExpression("i"),
+            lowerbound=ConstantExpression(1),
+            upperbound=VariableExpression("N")
+        )
+
+        assert check_plain_nested_loop(outer_loop)
+
+    def test_not_plain_nested_loop(self):
+        inner_loop = ForLoopStatement(
+            body=[AssigmentStatement(VariableExpression("a", [VariableExpression("j")]),
+                                     VariableExpression("b", [VariableExpression("j")]))],
+            index=VariableExpression("j"),
+            lowerbound=VariableExpression("i"),
+            upperbound=VariableExpression("M")
+        )
+
+        outer_loop = ForLoopStatement(
+            body=[inner_loop],
+            index=VariableExpression("i"),
+            lowerbound=ConstantExpression(1),
+            upperbound=VariableExpression("N")
+        )
+
+        assert not check_plain_nested_loop(outer_loop)
