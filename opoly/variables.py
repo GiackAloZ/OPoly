123c758fd276f6202e9902b467ecc17820b9a890
Giacomo Aloisi <giacomo.aloisi1998@gmail.com>
2021-01-03 00:11:20 +0100

    Add variable occurrences

---
+++ opoly/variables.py
@@ -0,0 +1,56 @@
+from enum import Enum, auto
+from abc import ABC, abstractmethod
+
+
+class VariableOccurrenceType(Enum):
+    GENERATION = auto()
+    USE = auto()
+
+
+class VariableOccurrence(ABC):
+
+    def __init__(self, name: str, vtype: VariableOccurrenceType):
+        self._vtype = vtype
+        self._name = name
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def vtype(self) -> VariableOccurrenceType:
+        return self._vtype
+
+    @abstractmethod
+    def stringify(self) -> str:
+        vtype_str = {
+            VariableOccurrenceType.GENERATION: "GEN",
+            VariableOccurrenceType.USE: "USE",
+        }
+        return f"[{vtype_str[self.vtype]}] VAR {self.name}"
+
+    def __str__(self):
+        return self.stringify()
+
+
+class SimpleVariableOccurrence(VariableOccurrence):
+
+    def __init__(self, name: str, vtype: VariableOccurrenceType, indexes: tuple[tuple[str, int]]):
+        super().__init__(name, vtype)
+        self._indexes = indexes
+
+    @property
+    def indexes(self) -> tuple[tuple[str, int]]:
+        return self._indexes
+
+    @property
+    def indexes_names(self) -> tuple[str]:
+        return tuple(idx for idx, offset in self._indexes)
+
+    @property
+    def offsets(self) -> tuple[int]:
+        return tuple(offset for idx, offset in self._indexes)
+
+    def stringify(self) -> str:
+        return (f"{super().stringify()}"
+                f"{''.join([f'[{idx}{offset:+}]' for idx, offset in self.indexes])}")
---
+++ tests/test_variables.py
@@ -0,0 +1,37 @@
+from opoly.variables import SimpleVariableOccurrence, VariableOccurrenceType
+
+
+class TestSimpleVariableOccurrence():
+
+    def test_simple(self):
+        occ = SimpleVariableOccurrence(
+            name="a",
+            vtype=VariableOccurrenceType.GENERATION,
+            indexes=(
+                ("i", 1),
+                ("j", -1)
+            )
+        )
+        assert str(occ) == "[GEN] VAR a[i+1][j-1]"
+
+    def test_indexes_names(self):
+        occ = SimpleVariableOccurrence(
+            name="a",
+            vtype=VariableOccurrenceType.GENERATION,
+            indexes=(
+                ("i", 1),
+                ("j", -1)
+            )
+        )
+        assert occ.indexes_names == ("i", "j")
+
+    def test_offsets(self):
+        occ = SimpleVariableOccurrence(
+            name="a",
+            vtype=VariableOccurrenceType.GENERATION,
+            indexes=(
+                ("i", 1),
+                ("j", -1)
+            )
+        )
+        assert occ.offsets == (1, -1)
