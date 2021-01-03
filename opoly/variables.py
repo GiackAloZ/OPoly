35d5eb0c868fe75efe86d42d6615eda95cf9e1fc
GiackAloZ <giacomo.aloisi1998@gmail.com>
2021-01-03 16:19:12 +0100

    Add variable descriptor and set

--- opoly/variables.py
+++ opoly/variables.py
@@ -1,5 +1,6 @@
 from enum import Enum, auto
 from abc import ABC, abstractmethod
+from typing import Optional
 
 
 class VariableOccurrenceType(Enum):
@@ -54,3 +55,66 @@ class SimpleVariableOccurrence(VariableOccurrence):
     def stringify(self) -> str:
         return (f"{super().stringify()}"
                 f"{''.join([f'[{idx}{offset:+}]' for idx, offset in self.indexes])}")
+
+
+class VariableDescriptorType(Enum):
+    ANY = "*"
+    POSITIVE = "+"
+    CONSTANT = ""
+
+
+class VariableDescriptor():
+
+    def __init__(self, vdtype: VariableDescriptorType, value: Optional[int] = None):  # pylint: disable=unsubscriptable-object
+        self._vdtype = vdtype
+        if vdtype == VariableDescriptorType.CONSTANT and value is None:
+            raise ValueError(
+                "Value cannot be None for constant type descriptor!")
+        if vdtype != VariableDescriptorType.CONSTANT and value is not None:
+            raise ValueError(
+                "Value must be None for non constant type descriptor!")
+        self._value = value
+
+    @property
+    def vdtype(self) -> VariableDescriptorType:
+        return self._vdtype
+
+    @property
+    def converted_value(self) -> int:
+        return self._value if self.vdtype == VariableDescriptorType.CONSTANT else 1
+
+    def __str__(self):
+        return f"{self.vdtype.value}{self._value if self._value is not None else ''}"
+
+    def __repr__(self):
+        return f"{self.converted_value}"
+
+
+class VariableSet():
+
+    def __init__(self, descriptors: tuple[VariableDescriptor]):
+        if len(descriptors) == 0:
+            raise ValueError("There must be at least one variable descriptor!")
+        self._descriptors = descriptors
+
+    @property
+    def descriptors(self) -> tuple[VariableDescriptor]:
+        return self._descriptors
+
+    @property
+    def converted_values(self) -> tuple[int]:
+        return tuple(descriptor.converted_value for descriptor in self.descriptors)
+
+    def is_positive(self) -> bool:
+        for v in self.converted_values:
+            if v > 0:
+                return True
+            if v < 0:
+                return False
+        return False
+
+    def __str__(self):
+        return f"({','.join([str(descriptor) for descriptor in self.descriptors])})"
+
+    def __repr__(self):
+        return f"({','.join([repr(descriptor) for descriptor in self.descriptors])})"
