c19d98bff62080e04a5c1af721e69243983ad067
GiackAloZ <giacomo.aloisi1998@gmail.com>
2021-02-05 17:23:25 +0100

    Fix expression parsing and add assignment parsing

--- opoly/modules/parser.py
+++ opoly/modules/parser.py
@@ -1,5 +1,5 @@
-from opoly.expressions import Expression, ConstantExpression, VariableExpression
-
+from opoly.expressions import Expression, ConstantExpression, VariableExpression, GroupingExpression
+from opoly.statements import ForLoopStatement, AssignmentStatement
 
 def isfloat(s: str) -> bool:
     try:
@@ -12,13 +12,13 @@ def isfloat(s: str) -> bool:
 
 def isint(s: str):
     try:
-        a = float(s)
-        b = int(a)
+        _ = int(s)
     except (TypeError, ValueError):
         return False
     else:
-        return a == b
+        return True
 
+## TODO fix every return value with Optional[]
 
 def parse_constant_expression(code: str) -> (ConstantExpression, str):
     if len(code) == 0:
@@ -48,7 +48,7 @@ def parse_variable_expression(code: str) -> (VariableExpression, str):
     code = code[i:]
     indexes = []
     while len(code) > 0 and code[0] == "[":
-        var_index, rem_code = parse_expression(code[1:].strip())
+        var_index, rem_code = parse_expression(code[1:].strip(), term_char="]")
         if var_index is None:
             return None, rem_code
         rem_code = rem_code.strip()
@@ -70,31 +70,75 @@ def parse_operator(code: str) -> (str, str):
     return code[0], code[1:]
 
 
-def parse_expression(code: str) -> (Expression, str):
-    if len(code) == 0:
-        return None, "Expected expression"
+def parse_grouping_expression(code: str) -> (GroupingExpression, str):
+    if len(code) == 0 or code[0] != "(":
+        return None, "Expected ("
+    expr, rem_code = parse_expression(code[1:], term_char=")")
+    if expr is None:
+        return None, rem_code
+    rem_code = rem_code.strip()
+    if len(rem_code) == 0 or rem_code[0] != ")":
+        return None, "Expected )"
+    return GroupingExpression([expr], []), rem_code[1:]
+
+
+def parse_expression(code: str, term_char=None) -> (Expression, str):
     terms = []
     operators = []
     while True:
+        # Check for expression
+        if len(code) == 0:
+            return None, "Expected expression"
+
+        # Check different subexpression terms
         if code[0].isalpha():
             next_term, code = parse_variable_expression(code.strip())
         elif code[0].isnumeric():
             next_term, code = parse_constant_expression(code.strip())
         elif code[0] == "(":
-            raise NotImplementedError()
+            next_term, code = parse_grouping_expression(code.strip())
         else:
             return None, "Unsupported expression term"
         if next_term is None:
             return None, code
         terms.append(next_term)
-        code = code.strip()
-        if len(code) == 0 or code[0] == "]" or code[0] == ")":
-            break
-        next_operator, code = parse_operator(code)
-        if next_operator is None:
-            return None, code
-        operators.append(next_operator)
+
+        # Check for end
         code = code.strip()
         if len(code) == 0:
-            return None, "Expected expression"
+            break
+
+        # Check for operator
+        next_operator, rem_code = parse_operator(code)
+        if next_operator is not None:
+            operators.append(next_operator)
+            code = rem_code.strip()
+            continue
+        if next_operator is None and term_char is not None and code[0] != term_char and term_char in code:
+            return None, rem_code
+
+        # Check for term char
+        if term_char is not None:
+            if code[0] == term_char:
+                break
+            return None, f"Expected {term_char}"
+        
+        return None, rem_code
     return Expression(terms, operators), code
+
+def parse_assignment_statement(code: str) -> (AssignmentStatement, str):
+    left, *more = code.split("=")
+    if len(more) == 0:
+        return None, "Expected = operator"
+    if len(more) > 1:
+        return None, "Expected only one = operator in assignment"
+    right = more[0]
+    left_expr, rem = parse_variable_expression(left.strip())
+    if left_expr is None:
+        return None, rem
+    if len(rem) > 0:
+        return None, "Expected only one variable expression for left term"
+    right_expr, rem = parse_expression(right.strip())
+    if right_expr is None:
+        return None, rem
+    return AssignmentStatement(left_expr, right_expr), None
--- tests/modules/test_parser.py
+++ tests/modules/test_parser.py
@@ -1,5 +1,12 @@
 from opoly.expressions import ConstantExpression, VariableExpression
-from opoly.modules.parser import parse_constant_expression, parse_variable_expression, parse_expression
+from opoly.modules.parser import (
+    parse_constant_expression,
+    parse_variable_expression,
+    parse_grouping_expression,
+    parse_operator,
+    parse_expression,
+    parse_assignment_statement
+)
 
 
 class TestParsingFunctions():
@@ -9,6 +16,7 @@ class TestParsingFunctions():
             "0": (0, ""),
             "1": (1, ""),
             "0.5": (0.5, ""),
+            "3.0": (3.0, ""),
             "10": (10, ""),
             "1000000": (1000000, ""),
             "1 / 2": (1, " / 2"),
@@ -29,7 +37,7 @@ class TestParsingFunctions():
         for code in bads:
             parsed, rem = parse_constant_expression(code)
             assert parsed is None
-    
+
     def test_simple_variable_parser(self):
         goods = {
             "a": ("a", ""),
@@ -40,6 +48,7 @@ class TestParsingFunctions():
             "a1": ("a", "1")
         }
         bads = {
+            "": "Expected variable expression",
             "0": "Expected variable expression",
             "1": "Expected variable expression",
             "-1": "Expected variable expression",
@@ -55,7 +64,7 @@ class TestParsingFunctions():
             parsed, error = parse_variable_expression(code)
             assert parsed is None
             assert error == err
-    
+
     def test_indexed_variable_parser(self):
         goods = {
             "a[1]": ("a[1]", ""),
@@ -84,12 +93,61 @@ class TestParsingFunctions():
             assert parsed is None
             assert error == err
     
+    def test_operator_parser(self):
+        goods = {
+            "+": ("+", ""),
+            "-": ("-", ""),
+            "*": ("*", ""),
+            "/": ("/", ""),
+            "/ i": ("/", " i"),
+        }
+        bads = {
+            "": "Expected operator"
+        }
+        for code, res in goods.items():
+            parsed, rem = parse_operator(code)
+            assert parsed is not None
+            assert str(parsed) == res[0]
+            assert rem == res[1]
+        for code, err in bads.items():
+            parsed, error = parse_operator(code)
+            assert parsed is None
+            assert error == err
+
+    def test_grouping_parser(self):
+        goods = {
+            "(a)": ("(a)", ""),
+            "(a + b)": ("(a + b)", ""),
+            "(a +b)": ("(a + b)", ""),
+            "(a+b)": ("(a + b)", ""),
+            "(a[i] + 1)": ("(a[i] + 1)", ""),
+            "(a[i] + b[i][j+1]) / 3.0": ("(a[i] + b[i][j + 1])", " / 3.0")
+        }
+        bads = {
+            "": "Expected (",
+            "(": "Expected expression",
+            ")": "Expected (",
+            "(a": "Expected )",
+            "(a a)": "Unsupported operator",
+            "(a + )": "Unsupported expression term"
+        }
+        for code, res in goods.items():
+            parsed, rem = parse_grouping_expression(code)
+            assert parsed is not None
+            assert str(parsed) == res[0]
+            assert rem == res[1]
+        for code, err in bads.items():
+            parsed, error = parse_grouping_expression(code)
+            assert parsed is None
+            assert error == err
+
     def test_expression_parser(self):
         goods = {
             "a + i": ("a + i", ""),
             "a[i] + b[i]": ("a[i] + b[i]", ""),
             "a[i] + b[i] / 3": ("a[i] + b[i] / 3", ""),
-            "a[i+1][j+1] + a[i-1][j-1] / 3": ("a[i + 1][j + 1] + a[i - 1][j - 1] / 3", "")
+            "a[i+1][j+1] + a[i-1][j-1] / 3": ("a[i + 1][j + 1] + a[i - 1][j - 1] / 3", ""),
+            "(a[i+1][j+1] + a[i-1][j-1]) / 3.0": ("(a[i + 1][j + 1] + a[i - 1][j - 1]) / 3.0", ""),
         }
         bads = {
             "": "Expected expression",
@@ -98,14 +156,53 @@ class TestParsingFunctions():
             "a+[": "Unsupported expression term",
             "[": "Unsupported expression term",
             "+": "Unsupported expression term",
-            "a a": "Unsupported operator"
+            "a a": "Unsupported operator",
+            "]": "Unsupported expression term",
+            "a]": "Unsupported operator",
+            "[]": "Unsupported expression term",
+            "(": "Expected expression"
         }
         for code, res in goods.items():
             parsed, rem = parse_expression(code)
             assert parsed is not None
-            assert str(parsed) == str(VariableExpression(res[0]))
+            assert str(parsed) == res[0]
             assert rem == res[1]
         for code, err in bads.items():
             parsed, error = parse_expression(code)
+            assert parsed is None
+            assert error == err
+        
+        parsed, err = parse_expression("a,", term_char=")")
+        assert parsed is None
+        assert err == "Expected )"
+
+    def test_assignment_statement_parser(self):
+        goods = {
+            "a = a + i": ("a = a + i", None),
+            "a[i] = a + i": ("a[i] = a + i", None),
+            "a[i+1] = a + i": ("a[i + 1] = a + i", None),
+            "a = a[i] + b[i]": ("a = a[i] + b[i]", None),
+            "a =a[i] + b[i] / 3": ("a = a[i] + b[i] / 3", None),
+            "a=a[i+1][j+1] + a[i-1][j-1] / 3": ("a = a[i + 1][j + 1] + a[i - 1][j - 1] / 3", None),
+            "a= (a[i+1][j+1] + a[i-1][j-1]) / 3.0": ("a = (a[i + 1][j + 1] + a[i - 1][j - 1]) / 3.0", None),
+        }
+        bads = {
+            "": "Expected = operator",
+            "a = b = c": "Expected only one = operator in assignment",
+            "= a": "Expected variable expression",
+            "a = ": "Expected expression",
+            "a + a = a": "Expected only one variable expression for left term",
+            "a = bc[": "Expected expression",
+            "a = asd + asd +": "Expected expression",
+            "a = ]": "Unsupported expression term",
+        }
+        for code, res in goods.items():
+            parsed, rem = parse_assignment_statement(code)
+            print(rem)
+            assert parsed is not None
+            assert str(parsed) == res[0]
+            assert rem == res[1]
+        for code, err in bads.items():
+            parsed, error = parse_assignment_statement(code)
             assert parsed is None
             assert error == err
