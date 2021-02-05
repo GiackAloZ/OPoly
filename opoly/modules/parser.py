c48977db7af2337a069687d998e85bf01ffba0a9
GiackAloZ <giacomo.aloisi1998@gmail.com>
2021-02-05 12:47:17 +0100

    Add variable and expression parser

--- opoly/modules/parser.py
+++ opoly/modules/parser.py
@@ -1,4 +1,5 @@
-from opoly.expressions import ConstantExpression
+from opoly.expressions import Expression, ConstantExpression, VariableExpression
+
 
 def isfloat(s: str) -> bool:
     try:
@@ -7,6 +8,7 @@ def isfloat(s: str) -> bool:
         return False
     else:
         return True
+
 
 def isint(s: str):
     try:
@@ -17,7 +19,10 @@ def isint(s: str):
     else:
         return a == b
 
+
 def parse_constant_expression(code: str) -> (ConstantExpression, str):
+    if len(code) == 0:
+        return None, "Expected constant expression"
     i = 0
     while i < len(code) and (code[i].isnumeric() or code[i] == "."):
         i += 1
@@ -27,5 +32,69 @@ def parse_constant_expression(code: str) -> (ConstantExpression, str):
     elif isfloat(res_str):
         res = float(res_str)
     else:
-        return None, code
+        return None, "Expected constant expression"
     return ConstantExpression(res), code[i:]
+
+
+def parse_variable_expression(code: str) -> (VariableExpression, str):
+    if len(code) == 0:
+        return None, "Expected variable expression"
+    i = 0
+    while i < len(code) and (code[i].isalpha() or code[i] == "_"):
+        i += 1
+    name = code[:i]
+    if name == "":
+        return None, "Expected variable expression"
+    code = code[i:]
+    indexes = []
+    while len(code) > 0 and code[0] == "[":
+        var_index, rem_code = parse_expression(code[1:].strip())
+        if var_index is None:
+            return None, rem_code
+        rem_code = rem_code.strip()
+        if len(rem_code) == 0 or rem_code[0] != "]":
+            return None, "Expected ]"
+        indexes.append(var_index)
+        code = rem_code[1:]
+    return VariableExpression(name, indexes), code
+
+
+def parse_operator(code: str) -> (str, str):
+    if len(code) == 0:
+        return None, "Expected operator"
+    supported_operators = [
+        "+", "-", "*", "/"
+    ]
+    if code[0] not in supported_operators:
+        return None, "Unsupported operator"
+    return code[0], code[1:]
+
+
+def parse_expression(code: str) -> (Expression, str):
+    if len(code) == 0:
+        return None, "Expected expression"
+    terms = []
+    operators = []
+    while True:
+        if code[0].isalpha():
+            next_term, code = parse_variable_expression(code.strip())
+        elif code[0].isnumeric():
+            next_term, code = parse_constant_expression(code.strip())
+        elif code[0] == "(":
+            raise NotImplementedError()
+        else:
+            return None, "Unsupported expression term"
+        if next_term is None:
+            return None, code
+        terms.append(next_term)
+        code = code.strip()
+        if len(code) == 0 or code[0] == "]" or code[0] == ")":
+            break
+        next_operator, code = parse_operator(code)
+        if next_operator is None:
+            return None, code
+        operators.append(next_operator)
+        code = code.strip()
+        if len(code) == 0:
+            return None, "Expected expression"
+    return Expression(terms, operators), code
--- tests/modules/test_parser.py
+++ tests/modules/test_parser.py
@@ -1,5 +1,5 @@
-from opoly.expressions import ConstantExpression
-from opoly.modules.parser import parse_constant_expression
+from opoly.expressions import ConstantExpression, VariableExpression
+from opoly.modules.parser import parse_constant_expression, parse_variable_expression, parse_expression
 
 
 class TestParsingFunctions():
@@ -22,12 +22,90 @@ class TestParsingFunctions():
         ]
         for code, res in goods.items():
             parsed, rem = parse_constant_expression(code)
-            print(parsed, rem)
             assert parsed is not None
             assert parsed.value == ConstantExpression(res[0]).value
             assert type(parsed.value) == type(ConstantExpression(res[0]).value)
             assert rem == res[1]
         for code in bads:
             parsed, rem = parse_constant_expression(code)
-            print(parsed, rem)
             assert parsed is None
+    
+    def test_simple_variable_parser(self):
+        goods = {
+            "a": ("a", ""),
+            "alpha": ("alpha", ""),
+            "a_b": ("a_b", ""),
+            "a + 1": ("a", " + 1"),
+            "a+1": ("a", "+1"),
+            "a1": ("a", "1")
+        }
+        bads = {
+            "0": "Expected variable expression",
+            "1": "Expected variable expression",
+            "-1": "Expected variable expression",
+            "1a": "Expected variable expression",
+            "[asd]": "Expected variable expression"
+        }
+        for code, res in goods.items():
+            parsed, rem = parse_variable_expression(code)
+            assert parsed is not None
+            assert str(parsed) == str(VariableExpression(res[0]))
+            assert rem == res[1]
+        for code, err in bads.items():
+            parsed, error = parse_variable_expression(code)
+            assert parsed is None
+            assert error == err
+    
+    def test_indexed_variable_parser(self):
+        goods = {
+            "a[1]": ("a[1]", ""),
+            "a[1][1]": ("a[1][1]", ""),
+            "a[i]": ("a[i]", ""),
+            "a[i+1]": ("a[i + 1]", ""),
+            "a[i+1][j+1] + 1": ("a[i + 1][j + 1]", " + 1"),
+            "a[a[i]]": ("a[a[i]]", ""),
+            "a[i] + 1": ("a[i]", " + 1"),
+            "a[i]+1": ("a[i]", "+1"),
+            "a]": ("a", "]")
+        }
+        bads = {
+            "a[": "Expected expression",
+            "a[]": "Unsupported expression term",
+            "a[[]": "Unsupported expression term",
+            "a[a[i]": "Expected ]"
+        }
+        for code, res in goods.items():
+            parsed, rem = parse_variable_expression(code)
+            assert parsed is not None
+            assert str(parsed) == str(VariableExpression(res[0]))
+            assert rem == res[1]
+        for code, err in bads.items():
+            parsed, error = parse_variable_expression(code)
+            assert parsed is None
+            assert error == err
+    
+    def test_expression_parser(self):
+        goods = {
+            "a + i": ("a + i", ""),
+            "a[i] + b[i]": ("a[i] + b[i]", ""),
+            "a[i] + b[i] / 3": ("a[i] + b[i] / 3", ""),
+            "a[i+1][j+1] + a[i-1][j-1] / 3": ("a[i + 1][j + 1] + a[i - 1][j - 1] / 3", "")
+        }
+        bads = {
+            "": "Expected expression",
+            "a +": "Expected expression",
+            "a+": "Expected expression",
+            "a+[": "Unsupported expression term",
+            "[": "Unsupported expression term",
+            "+": "Unsupported expression term",
+            "a a": "Unsupported operator"
+        }
+        for code, res in goods.items():
+            parsed, rem = parse_expression(code)
+            assert parsed is not None
+            assert str(parsed) == str(VariableExpression(res[0]))
+            assert rem == res[1]
+        for code, err in bads.items():
+            parsed, error = parse_expression(code)
+            assert parsed is None
+            assert error == err
