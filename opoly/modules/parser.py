966e13d8c807408d0a1b97f409023368eeb679ec
GiackAloZ <giacomo.aloisi1998@gmail.com>
2021-02-06 14:27:36 +0100

    Add pseudocode loop parser (more tests needed)

--- opoly/modules/parser.py
+++ opoly/modules/parser.py
@@ -1,5 +1,8 @@
+from abc import ABC, abstractmethod
+import re
+
 from opoly.expressions import Expression, ConstantExpression, VariableExpression, GroupingExpression
-from opoly.statements import ForLoopStatement, AssignmentStatement
+from opoly.statements import Statement, ForLoopStatement, AssignmentStatement
 
 
 def isfloat(s: str) -> bool:
@@ -145,3 +148,102 @@ def parse_assignment_statement(code: str) -> (AssignmentStatement, str):
     if right_expr is None:
         return None, rem
     return AssignmentStatement(left_expr, right_expr), None
+
+
+class ForLoopParser(ABC):
+
+    @abstractmethod
+    def parse_for_loop(self, code: str) -> (ForLoopStatement, str):
+        pass
+
+
+class PseudocodeForLoopParser(ABC):
+
+    HEADER_REGEX = re.compile(
+        r"^FOR\s+(?P<idx>.+?)\s+FROM\s+(?P<lb>.+?)\s+TO\s+(?P<ub>.+?)(?:\s+STEP\s+(?P<step>.+?))?\s*\{\s*(?P<body>.*)\s*\}"
+    )
+
+    ASSIGNMENT_REGEX = re.compile(
+        r"^VAR\s+(?P<ass>.*?\s*=\s*.*?);"
+    )
+
+    def parse_loop_body(self, code: str) -> (tuple[Statement], str):
+        code = code.strip()
+        body_stmts = []
+        while len(code) > 0:
+            match = self.HEADER_REGEX.match(code)
+            if match is not None:
+                start, end = match.span()
+                stmt, err = self.parse_for_loop(code[start:end])
+                if stmt is None:
+                    return None, err
+                code = code[end:]
+                body_stmts.append(stmt)
+                continue
+
+            match = self.ASSIGNMENT_REGEX.match(code)
+            if match is not None:
+                start, end = match.span()
+                assignment_code = match.groupdict()["ass"].strip()
+                stmt, err = parse_assignment_statement(assignment_code)
+                if stmt is None:
+                    return None, err
+                code = code[end+1:]
+                body_stmts.append(stmt)
+                continue
+
+            return None, "Unsupported statement"
+        return body_stmts, None
+
+    def parse_for_loop(self, code: str) -> (ForLoopStatement, str):
+        code = code.strip().replace("\n", " ")
+        # Parse header
+        if len(code) == 0:
+            return None, "Expected loop header"
+        match = self.HEADER_REGEX.match(code)
+        if match is None:
+            return None, "Incorrect loop header syntax"
+
+        # Parse header parameters
+        groups_dict = match.groupdict()
+        idx_str = groups_dict["idx"]
+        lb_str = groups_dict["lb"]
+        ub_str = groups_dict["ub"]
+        step_str = groups_dict["step"]
+        idx_var_expr, err = parse_variable_expression(idx_str)
+        if idx_var_expr is None:
+            return None, err
+        lb_expr, err = parse_expression(lb_str)
+        if lb_expr is None:
+            return None, err
+        ub_expr, err = parse_expression(ub_str)
+        if ub_expr is None:
+            return None, err
+        step_expr = None
+        if step_str is not None:
+            step_expr, err = parse_expression(step_str)
+            if step_expr is None:
+                return None, err
+
+        # Parse body
+        body_str = groups_dict["body"].strip()
+        if body_str is None or len(body_str) == 0:
+            return None, "Expected loop body"
+        body, err = self.parse_loop_body(body_str)
+        if body is None:
+            return None, err
+
+        if step_expr is not None:
+            return ForLoopStatement(
+                body=body,
+                index=idx_var_expr,
+                lowerbound=lb_expr,
+                upperbound=ub_expr,
+                step=step_expr
+            ), None
+        return ForLoopStatement(
+            body=body,
+            index=idx_var_expr,
+            lowerbound=lb_expr,
+            upperbound=ub_expr
+        ), None
--- tests/modules/test_parser.py
+++ tests/modules/test_parser.py
@@ -5,7 +5,8 @@ from opoly.modules.parser import (
     parse_grouping_expression,
     parse_operator,
     parse_expression,
-    parse_assignment_statement
+    parse_assignment_statement,
+    PseudocodeForLoopParser
 )
 
 
@@ -206,3 +207,99 @@ class TestParsingFunctions():
             parsed, error = parse_assignment_statement(code)
             assert parsed is None
             assert error == err
+
+
+class TestPseudocodeForLoopParser():
+
+    def test_simple_parse_loop_body(self):
+        code = "VAR a[i] = a[i+1];"
+        stmts, err = PseudocodeForLoopParser().parse_loop_body(code)
+        print(err)
+        assert stmts is not None
+        assert len(stmts) == 1
+        assert str(stmts[0]) == "a[i] = a[i + 1]"
+
+    def test_long_parse_loop_body(self):
+        code = "VAR a = 10;\nVAR b = a;\nVAR c[x] = a+b;"
+        stmts, err = PseudocodeForLoopParser().parse_loop_body(code)
+        print(err)
+        assert stmts is not None
+        assert len(stmts) == 3
+        assert str(stmts[0]) == "a = 10"
+        assert str(stmts[1]) == "b = a"
+        assert str(stmts[2]) == "c[x] = a + b"
+
+    def test_parse_1d_loop(self):
+        code = "FOR i FROM 0 TO n { VAR a[i]=a[i+1]; }"
+        loop, err = PseudocodeForLoopParser().parse_for_loop(code)
+        print(err)
+        assert loop is not None
+        assert len(loop.body) == 1
+        assert str(loop.index) == "i"
+        assert str(loop.lowerbound) == "0"
+        assert str(loop.upperbound) == "n"
+        assert str(loop.step) == "1"
+        assert str(loop.body[0]) == "a[i] = a[i + 1]"
+
+    def test_parse_plain_nested_2d_loop(self):
+        code = "FOR i FROM 1 TO N - 1 STEP 1 { FOR j FROM 2 TO M-1 { VAR a[j] = (a[j-1] + a[j] + a[j+1]) / 3.0; } }"
+        loop, err = PseudocodeForLoopParser().parse_for_loop(code)
+        print(err)
+        assert loop is not None
+        assert len(loop.body) == 1
+        assert str(loop.index) == "i"
+        assert str(loop.lowerbound) == "1"
+        assert str(loop.upperbound) == "N - 1"
+        assert str(loop.step) == "1"
+        inner_loop = loop.body[0]
+        assert inner_loop is not None
+        assert len(inner_loop.body) == 1
+        assert str(inner_loop.index) == "j"
+        assert str(inner_loop.lowerbound) == "2"
+        assert str(inner_loop.upperbound) == "M - 1"
+        assert str(inner_loop.step) == "1"
+        assert str(
+            inner_loop.body[0]) == "a[j] = (a[j - 1] + a[j] + a[j + 1]) / 3.0"
+
+    def test_wrong_inner_loop(self):
+        code = "FOR i FROM 1 TO N - 1 STEP 1 { FOR j FROM 2 TO M { VAR a[j] == (a[j-1] + a[j] + a[j+1]) / 3.0; } }"
+        loop, err = PseudocodeForLoopParser().parse_for_loop(code)
+        assert loop is None
+
+    def test_unsupported_inner_statement_loop(self):
+        code = "FOR i FROM 0 TO n { asd }"
+        loop, err = PseudocodeForLoopParser().parse_for_loop(code)
+        assert loop is None
+
+    def test_expected_loop(self):
+        code = ""
+        loop, err = PseudocodeForLoopParser().parse_for_loop(code)
+        assert loop is None
+        code = "asdasd"
+        loop, err = PseudocodeForLoopParser().parse_for_loop(code)
+        assert loop is None
+
+    def test_wrong_loop_index(self):
+        code = "FOR 2 FROM 1 TO N {a = 1;}"
+        loop, err = PseudocodeForLoopParser().parse_for_loop(code)
+        assert loop is None
+
+    def test_wrong_lowerbound(self):
+        code = "FOR i FROM 2**2 TO N {VAR a = 1;}"
+        loop, err = PseudocodeForLoopParser().parse_for_loop(code)
+        assert loop is None
+
+    def test_wrong_upperbound(self):
+        code = "FOR i FROM 1 TO N//2 {VAR a = 1;}"
+        loop, err = PseudocodeForLoopParser().parse_for_loop(code)
+        assert loop is None
+
+    def test_wrong_step(self):
+        code = "FOR i FROM 1 TO N/2 STEP 2**2 {VAR a = 1;}"
+        loop, err = PseudocodeForLoopParser().parse_for_loop(code)
+        assert loop is None
+
+    def test_empty_body(self):
+        code = "FOR i FROM 1 TO N/2 STEP j { }"
+        loop, err = PseudocodeForLoopParser().parse_for_loop(code)
+        assert loop is None
